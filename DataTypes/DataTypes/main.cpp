2e1d9d5415019a0d8d27c0acb7c5cd46fca18295
Clayman <Clayman@DESKTOP-DM8E77U>
2022-09-30 16:19:10 +0300

    DataTypes day 1

--- DataTypes/DataTypes/main.cpp
+++ DataTypes/DataTypes/main.cpp
@@ -1,10 +1,18 @@
 #include<iostream>
 using namespace std;
 
+//#define LOGICAL_TYPES
+
 void main()
 {
 	setlocale(LC_ALL, "Rus");
+#if defined LOGICAL_TYPES
 	cout << "DataTypes" << endl;
 	cout << true << endl;
 	cout << false << endl;
+	//bit - Binary Digit (Двоичная цифра) 0 1
+#endif
+
+	//char (Character - Символ) 1 Byte
+	//ASCII - American Standard Code for Information Interchange
 }
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,6 +1,8 @@
 https://github.com/okovtun/BU_211.git
 https://www.youtube.com/watch?v=UpM9unB1Pgs&list=PLeqyOOqxeiIOawl8qOYJiv1-8oEHXZvZy
 
+Выучить теорию!!!!!!!!!!!!!!!!!!!!!
+
 TODO:
 1. Выучить теорию!
 2. Используя Escape-последовательности вывести в консоль стишок
