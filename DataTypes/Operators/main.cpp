cccc1e268281e55f1c9c93b3dec02a23fd025960
Clayman <Clayman@DESKTOP-DM8E77U>
2022-10-14 13:04:28 +0300

    Operators day 1

---
+++ DataTypes/Operators/main.cpp
@@ -0,0 +1,24 @@
+﻿//Operators
+#include<iostream>
+using namespace std;
+
+#define ARITHMETICAL_OPERATORS		//1) Арифметические операторы
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	cout << "Hello Operators" << endl;
+	3;
+	-3;		//Unary minus
+	8 - 3;	//Binary minus
+	8 * 3;	//Binary asterisk
+	//*3;	//Have no sense!!!
+
+	//Unary: +, -;
+	//Binary:+, -, *, /, %;
+	//		 % - остаток от деления (modulo)
+	cout << 17. / 5 << endl;
+	//cout << 17 % 5. << endl;
+	//Integer - целое число
+	//Integral - целочисленный
+}
