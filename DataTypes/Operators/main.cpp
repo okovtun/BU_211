0e336c8dcd41cc4852a5920bc80aa577c661b5c2
Clayman <Clayman@DESKTOP-DM8E77U>
2022-10-19 13:20:57 +0300

    Operators day 2

--- DataTypes/Operators/main.cpp
+++ DataTypes/Operators/main.cpp
@@ -1,12 +1,20 @@
 ﻿//Operators
 #include<iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
-#define ARITHMETICAL_OPERATORS		//1) Арифметические операторы
+//#define ARITHMETICAL_OPERATORS		//1) Арифметические операторы
+//#define ASSIGNMENT_OPERATOR			//2) Оператор присваивания = 
+//#define INCREMENT_DECREMENT			//3) Increment/Decrement(++/--)
+//#define COMPOUND_ASSIGNMENTS			//4) Составные присваивания
+#define COMPARISON_OPERATORS			//5) Операторы сравнения
 
 void main()
 {
 	setlocale(LC_ALL, "Rus");
+#ifdef ARITHMETICAL_OPERATORS
 	cout << "Hello Operators" << endl;
 	3;
 	-3;		//Unary minus
@@ -20,5 +28,58 @@ void main()
 	cout << 17. / 5 << endl;
 	//cout << 17 % 5. << endl;
 	//Integer - целое число
-	//Integral - целочисленный
+	//Integral - целочисленный  
+#endif // ARITHMETICAL_OPERATORS
+
+#ifdef ASSIGNMENT_OPERATOR
+			//			l-value = r-value;
+
+//int a = 2;	//переменной 'a' присваиваем значение '2' (Assign '2' to 'a')
+//int b = a;	//переменной 'b' присвоить значение переменной 'a'
+//int c = (a + b * 4) / 5;
+
+	2 + 3 + 8 + 4;
+
+	int a, b, c;
+	a = b = c = 0;
+	cout << a << "\t" << b << "\t" << c << endl;
+#endif // ASSIGNMENT_OPERATOR
+
+#ifdef INCREMENT_DECREMENT
+	//Increment (++) - это унарный оператор....
+//Prefix
+//Postfix
+	int i = 0;
+	++i;	//Prefix increment
+	i++;	//Postfix increment
+	--i;	//Prefix decrement
+	i--;	//Postfix (Suffix) decrement 
+
+	int j = ++i;
+
+	cout << i << endl;	//1
+	cout << j << endl;	//1  
+#endif // INCREMENT_DECREMENT
+
+#ifdef COMPOUND_ASSIGNMENTS
+	int a = 2;
+	int b = 3;
+
+	//a = a + b;		a += b;
+	//a = a - b;		a -= b;
+	//a = a * b;		a *= b;
+	//a = a / b;		a /= b;
+	//a = a % b;		a %= b;
+
+	a + b;	//Оператор + просто возвращает сумму
+	a += b;	//Оператор += возвращает сумму и сохраняет ее в операнд слева
+
+	a + b;	//Оператор "Сложить"
+	a += b;	//Оператор "Прибавить"
+
+	a - b;	//Оператор "Вычесть"
+	a -= b;	//Оператор "Отнять"  
+#endif // COMPOUND_ASSIGNMENTS
+
+	cout << (!true == false) << endl;
 }
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,7 +1,37 @@
 https://github.com/okovtun/BU_211.git
 https://www.youtube.com/watch?v=UpM9unB1Pgs&list=PLeqyOOqxeiIOawl8qOYJiv1-8oEHXZvZy
 
-Выучить теорию!!!!!!!!!!!!!!!!!!!!!
+TODO:
+1. Выучить теорию!!!!!!!!!!!!!!!!!!!!!
+2. Решить следующие задачи без исползования компилятора, 
+   в компиляторе их можно только проверить:
+
+   //1)
+   int i = 3;
+   i = ++i + ++i;
+   cout << i << endl;	//что будет выведено на экран
+
+   //2)
+   int i = 3;
+   i = ++i + 1 + ++i * 2;
+   cout << i << endl;	//что будет выведено на экран
+
+   //3)
+   int i = 3;
+   i = ++i + 1 + ++i *= 2;
+   cout << i << endl;	//что будет выведено на экран
+
+   //4)
+   int i = 3;
+   i += ++i + 1 + ++i *= 2;
+   cout << i << endl;	//что будет выведено на экран
+
+   //5)
+   int i = 3;
+   i *= ++i + 1 + ++i += 2;
+   cout << i << endl;	//что будет выведено на экран
+
+
 TOREAD:
 https://cplusplus.com/doc/tutorial/constants/
 https://github.com/okovtun/BU_211/blob/master/NumericSystems.pdf
