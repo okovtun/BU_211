b47f6894e2056b677baf0d3baace27521facb66c
Clayman <Clayman@DESKTOP-DM8E77U>
2022-11-30 11:12:18 +0200

    NestedFOR

--- ControlStructures/FOR/main.cpp
+++ ControlStructures/FOR/main.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
+#include<Windows.h>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
 //#define FOR_BASICS
-#define FACTORIAL
+//#define FACTORIAL
+//#define POWER
+//#define ASCII
+//#define FIBONACCI_1
+//#define SIMPLE_NUMERS
+//#define MULTIPLICATION_TABLE
 
 void main()
 {
@@ -32,4 +41,97 @@ void main()
 	}
 #endif // FACTORIAL
 
+#ifdef POWER
+	double a;		//Основание степени
+	int n;		//Показатель степени
+	double N = 1;	//Степень
+	cout << "Введите основание степени: "; cin >> a;
+	cout << "Введите показатель степени: "; cin >> n;
+	cout << a << " ^ " << n << " = ";
+	if (n < 0)
+	{
+		a = 1 / a;
+		n = -n;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		N *= a;
+	}
+	cout << N << endl;
+#endif // POWER
+
+#ifdef ASCII
+	cout << "Таблица ASCII-символов:\n";
+	setlocale(LC_ALL, "C");	//Устанавливает кодировку по умолчанию
+	for (int i = 0; i < 256; i++)
+	{
+		if (i % 16 == 0)cout << endl;
+		cout << (char)i << " ";
+	}
+	cout << endl;
+	setlocale(LC_ALL, "");
+	cout << "Вот и сказочке конец" << endl;
+#endif // ASCII
+
+#ifdef FIBONACCI_1
+	int n;
+	cout << "Введите предельно число: "; cin >> n;
+	for (int a = 0, b = 1, c = a + b; a < n; a = b, b = c, c = a + b)
+	{
+		cout << a << "\t";
+	}
+	cout << endl;
+#endif // FIBONACCI_1
+
+#ifdef SIMPLE_NUMERS
+	int n;
+	cout << "Введите предельное число: "; cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		bool simple = true;	//Изначально предположим, что число простое,
+		//но это нужно проверить:
+		for (int j = 2; j < i; j++)
+		{
+			if (i%j == 0)
+			{
+				simple = false;
+				break;
+			}
+		}
+		if (simple)cout << i << "\t";
+	}
+	cout << endl;
+#endif // SIMPLE_NUMERS
+
+#ifdef MULTIPLICATION_TABLE
+	for (int i = 1; i <= 10; i++)
+	{
+		for (int j = 1; j <= 10; j++)
+		{
+			cout << i << " * " << j << " = " << i * j << endl;
+		}
+	}
+#endif // MULTIPLICATION_TABLE
+
+	/*for (int h = 0; h < 24; h++)
+	{
+		for (int m = 0; m < 60; m++)
+		{
+			for (int s = 0; s < 60; s++)
+			{
+				system("CLS");
+				cout << h << ":" << m << ":" << s << endl;
+				Sleep(50);
+			}
+		}
+	}*/
+
+	for (int i = 1; i <= 10; i++)
+	{
+		for (int j = 1; j <= 10; j++)
+		{
+			cout << i * j << "\t";
+		}
+		cout << endl;
+	}
 }
