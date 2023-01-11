d3a20e5ced375dee75d84122c72745c5185422ab
Clayman <Clayman@DESKTOP-DM8E77U>
2023-01-11 12:55:07 +0300

    Numerics start

---
+++ Arrays/Numerics/main.cpp
@@ -0,0 +1,11 @@
+﻿#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int decimal;
+	cout << "Введите десятичное число: "; cin >> decimal;
+	const int n = 32;	//максимально возможная разрядность числа типа 'int'
+	bool bin[n] = {};	//bin - Binary, этот массив будет хранить разряды (биты) двоичного числа.
+}
