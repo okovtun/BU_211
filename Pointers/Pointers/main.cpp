0c3bb958bebb3793f9ad463f4e7b502e4218e305
Clayman <Clayman@DESKTOP-DM8E77U>
2023-03-03 14:37:43 +0300

    Pointers day 1

--- Pointers/Pointers/main.cpp
+++ Pointers/Pointers/main.cpp
@@ -1,8 +1,59 @@
 #include<iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+//#define POINTERS_BASICS
 
 void main()
 {
 	setlocale(LC_ALL, "");
 
+#ifdef POINTERS_BASICS
+	int a = 2;
+	int* pa = &a;	//pa - Poiter to 'a' (указатель на переменную 'a')
+	cout << a << endl;	//Вывод значения переменной 'a' на экран
+	cout << &a << endl;	//Взятие адреса переменной 'a' прямо при выводе
+	cout << pa << endl;	//Вывод адреса переменной 'a', хранящегося в указателе 'pa'
+	cout << *pa << endl;//Разыменование указателя 'pa', для того чтобы получить значение по адресу.
+
+	int* pb;
+	int b = 3;
+	pb = &b;
+	/*
+	------------------------------
+	int  - 'int'
+	int* - Указатель на 'int'
+	double  - 'double'
+	double* - Указатель на 'double'
+	------------------------------
+	*/
+#endif 
+
+	const int n = 5;
+	int arr[n] = { 3, 5, 8, 13, 21 };
+	cout << arr << endl;
+	cout << *arr << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << *(arr + i) << "\t";
+	}
+	cout << endl;
 }
+
+/*
+---------------------------------------------
+& - Оператор взятия адреса (Address-of operator)
+* - Оператор разыменования (Dereference operator)
+---------------------------------------------
+*/
+
+/*
+---------------------------------------------
++
+-
+++
+--
+---------------------------------------------
+*/
